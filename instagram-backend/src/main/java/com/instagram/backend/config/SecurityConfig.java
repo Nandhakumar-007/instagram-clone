af696a0581a862104070b69c895a9d10102a4a01
Nandhakumar <23h128@amjaincollege.edu.in>
2026-08-18 19:16:43 +0530

    Update Vercel CORS origin

--- instagram-backend/src/main/java/com/instagram/backend/config/SecurityConfig.java
+++ instagram-backend/src/main/java/com/instagram/backend/config/SecurityConfig.java
@@ -111,7 +111,7 @@ public class SecurityConfig {
 
         configuration.setAllowedOrigins(List.of(
                 "http://localhost:5173",
-                "https://instagram-clone-bgudn05ej-nandhakumar2.vercel.app"
+                "https://instagram-clone-ashen-eight.vercel.app"
         ));
 
         configuration.setAllowedMethods(List.of(
