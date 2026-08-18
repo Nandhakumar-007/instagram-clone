7bdb6562fc3cafce2ef5a376be60990bc950458f
NandhaKumar V <nandhakumarvenkatesan007@gmail.com>
2026-08-18 22:07:59 +0530

    Update SecurityConfig.java

--- instagram-backend/src/main/java/com/instagram/backend/config/SecurityConfig.java
+++ instagram-backend/src/main/java/com/instagram/backend/config/SecurityConfig.java
@@ -111,7 +111,7 @@ public class SecurityConfig {
 
         configuration.setAllowedOrigins(List.of(
                 "http://localhost:5173",
-                "https://instagram-clone-ashen-eight.vercel.app"
+                "https://instagram-clone-kxzxpzt6k-nandhakumar2.vercel.app"
         ));
 
         configuration.setAllowedMethods(List.of(
